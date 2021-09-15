f323ea9dbf97648684a1b806f7e06fe7b71adfd0
Gregory Macat <gregorymacat@gmail.com>
2021-09-15 14:31:12 -0400

    Solution partially found

--- algorithm2/day1/firstLastPos.js
+++ algorithm2/day1/firstLastPos.js
@@ -1,4 +1,5 @@
 /*
+https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
 
 If target is not found in the array, return [-1, -1].
@@ -35,8 +36,24 @@ nums is a non-decreasing array.
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-  //track the indexes of the found values
   var indices = [-1, -1];
+  var middle = Math.floor(nums.length / 2);
+  var low = nums[middle] < target ? middle : 0;
+  var high = nums[middle] > target ? middle : nums.length - 1;
+  //console.log('low ', low, ' high ', high);
+
+  while (low !== high && indices[0] === -1) {
+    //console.log(middle);
+    if (nums[middle] === target) {
+      indices[0] = middle;
+    } else if (nums[middle] < target) {
+      high = middle;
+      middle = Math.floor(middle / 2);
+    } else if (nums[middle] > target) {
+      low = middle;
+      middle = Math.floor((low + high) / 2);
+    }
+  }
 
   for (var index = 0; index < nums.length; index++) {
     //find the first index that matches the target
