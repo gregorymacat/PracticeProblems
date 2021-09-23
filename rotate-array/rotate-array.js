813d0115f4dba460c2fafee1b7ac3af0226f02f3
Gregory Macat <gregorymacat@gmail.com>
2021-09-23 02:56:46 -0400

    Added original close solution in comments

--- rotate-array/rotate-array.js
+++ rotate-array/rotate-array.js
@@ -4,6 +4,16 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
+//  if (nums.length === 1) {
+//    return nums;
+//  }
+    
+//  let currStep = 1;
+//  while (currStep <= k) {
+//    let last = nums.pop();
+//    nums.unshift(last);
+//    currStep++;
+//  }
     if (k > nums.length) {
         k = k % nums.length;
     }
